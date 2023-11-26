d054e799eead1facad82cea2a96726bccd9d005c
WilliamChidiacFunctori <william.chidiac@functori.com>
2023-11-26 05:01:50 +0100

    [format] formatted the code.

--- interpreter.ml
+++ interpreter.ml
@@ -1,38 +1,41 @@
 open Kawa
 
 type value =
-  | VInt  of int
+  | VInt of int
   | VBool of bool
-  | VObj  of obj
+  | VObj of obj
   | Null
+
 and obj = {
-  cls:    string;
-  fields: (string, value) Hashtbl.t;
+  cls : string;
+  fields : (string, value) Hashtbl.t;
 }
 
 exception Error of string
+
 exception Return of value
 
-let exec_prog (p: program): unit =
+let exec_prog (p : program) : unit =
   let env = Hashtbl.create 16 in
-  List.iter (fun (x, _) -> Hashtbl.add env x Null) p.globals;
-  
-  let rec eval_call f this args =
-    failwith "eval_call not implemented"
+  List.iter (fun (x, _) -> Hashtbl.add env x Null) p.globals ;
 
+  let rec eval_call f this args = failwith "eval_call not implemented"
   and exec_seq s lenv =
-    let rec evali e = match eval e with
+    let rec evali e =
+      match eval e with
       | VInt n -> n
       | _ -> assert false
-    and evalb e = match eval e with
+    and evalb e =
+      match eval e with
       | VBool b -> b
       | _ -> assert false
-    and evalo e = match eval e with
+    and evalo e =
+      match eval e with
       | VObj o -> o
       | _ -> assert false
-        
-    and eval (e: expr): value = match e with
-      | Int n  -> VInt n
+    and eval (e : expr) : value =
+      match e with
+      | Int n -> VInt n
       | Binop (Add, e1, e2) -> VInt (evali e1 + evali e2)
       | Binop (Sub, e1, e2) -> VInt (evali e1 - evali e2)
       | Binop (Mul, e1, e2) -> VInt (evali e1 * evali e2)
@@ -46,24 +49,21 @@ let exec_prog (p: program): unit =
       | Binop (Neq, e1, e2) -> VBool (evali e1 <> evali e2)
       | Binop (Or, e1, e2) -> VBool (evalb e1 || evalb e2)
       | Binop (And, e1, e2) -> VBool (evalb e1 && evalb e2)
-      | Unop (Opp, e) -> VInt(- evali e)
+      | Unop (Opp, e) -> VInt (-evali e)
       | Unop (Not, e) -> VBool (not (evalb e))
-      | _ -> failwith "case not implemented in eval"
-    in
-  
-    let rec exec (i: instr): unit = match i with
-      | Print e ->
-        begin match eval e with 
-        |VInt i ->  Printf.printf "%d\n" (i)
-        |VBool b -> Printf.printf "%b\n" b
-        | _ -> failwith "case not implemented in exec"
-        end
-      | _ -> failwith "case not implemented in exec"
-    and exec_seq s = 
-      List.iter exec s
-    in
+      | _ -> failwith "case not implemented in eval" in
 
-    exec_seq s
-  in
-  
+    let rec exec (i : instr) : unit =
+      match i with
+      | Print e -> begin
+        match eval e with
+        | VInt i -> Printf.printf "%d\n" i
+        | VBool b -> Printf.printf "%b\n" b
+        | _ -> failwith "case not implemented in exec"
+      end
+      | _ -> failwith "case not implemented in exec"
+    and exec_seq s = List.iter exec s in
+
+    exec_seq s in
+
   exec_seq p.main (Hashtbl.create 1)
--- kawa.ml
+++ kawa.ml
@@ -2,7 +2,7 @@
    Kawa : un petit langage à objets inspiré de Java
  *)
 
-(* Types déclarés pour les attributs, pour les variables, et pour les 
+(* Types déclarés pour les attributs, pour les variables, et pour les
    paramètres et résultats des méthodes. *)
 type typ =
   | TVoid
@@ -11,86 +11,100 @@ type typ =
   | TClass of string
 
 let typ_to_string = function
-  | TVoid    -> "void"
-  | TInt     -> "int"
-  | TBool    -> "bool"
+  | TVoid -> "void"
+  | TInt -> "int"
+  | TBool -> "bool"
   | TClass c -> c
 
-type unop  = Opp | Not
-type binop = Add | Sub | Mul | Div | Rem
-           | Lt  | Le  | Gt | Ge | Eq  | Neq
-           | And | Or
+type unop =
+  | Opp
+  | Not
+
+type binop =
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | Rem
+  | Lt
+  | Le
+  | Gt
+  | Ge
+  | Eq
+  | Neq
+  | And
+  | Or
 
 (* Expressions *)
 type expr =
   (* Base arithmétique *)
-  | Int    of int
-  | Bool   of bool
-  | Unop   of unop * expr
-  | Binop  of binop * expr * expr
+  | Int of int
+  | Bool of bool
+  | Unop of unop * expr
+  | Binop of binop * expr * expr
   (* Accès à une variable ou un attribut *)
-  | Get      of mem_access
+  | Get of mem_access
   (* Objet courant *)
   | This
   (* Création d'un nouvel objet *)
-  | New      of string
-  | NewCstr  of string * expr list
+  | New of string
+  | NewCstr of string * expr list
   (* Appel de méthode *)
   | MethCall of expr * string * expr list
 
 (* Accès mémoire : variable ou attribut d'un objet *)
 and mem_access =
-  | Var   of string
+  | Var of string
   | Field of expr (* objet *) * string (* nom d'un attribut *)
 
 (* Instructions *)
 type instr =
   (* Affichage d'un entier *)
-  | Print  of expr
+  | Print of expr
   (* Écriture dans une variable ou un attribut *)
-  | Set    of mem_access * expr
+  | Set of mem_access * expr
   (* Structures de contrôle usuelles *)
-  | If     of expr * seq * seq
-  | While  of expr * seq
+  | If of expr * seq * seq
+  | While of expr * seq
   (* Fin d'une fonction *)
   | Return of expr
   (* Expression utilisée comme instruction *)
-  | Expr   of expr
+  | Expr of expr
 
 and seq = instr list
 
-(* Définition de méthode 
+(* Définition de méthode
 
    Syntaxe : method <type de retour> <nom> (<params>) { ... }
 
    Le corps de la méthode est similaire au corps d'une fonction. *)
 type method_def = {
-    method_name: string;
-    code: seq;
-    params: (string * typ) list;
-    locals: (string * typ) list;
-    return: typ;
-  }
-        
-(* Définition de classe 
+  method_name : string;
+  code : seq;
+  params : (string * typ) list;
+  locals : (string * typ) list;
+  return : typ;
+}
+
+(* Définition de classe
 
    Syntaxe : class <nom de la classe> { ... }
         ou : class <nom de la classe> extends <nom de la classe mère> { ... }
 
    On considère que toute classe C contient une définition de méthode de nom
-   "constructor" et de type de retour void, qui initialise les champs du 
+   "constructor" et de type de retour void, qui initialise les champs du
    paramètre implicite this. *)
 type class_def = {
-    class_name: string;
-    attributes: (string * typ) list;
-    methods: method_def list;
-    parent: string option;
-  }
+  class_name : string;
+  attributes : (string * typ) list;
+  methods : method_def list;
+  parent : string option;
+}
 
-(* Programme complet : variables globales, classes, et une séquence 
+(* Programme complet : variables globales, classes, et une séquence
    d'instructions *)
 type program = {
-    classes: class_def list;
-    globals: (string * typ) list;
-    main: seq;
-  }
+  classes : class_def list;
+  globals : (string * typ) list;
+  main : seq;
+}
