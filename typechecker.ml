301b7b58954dd059d5cfae5cccaa04920b69d76b
Paul-Alexis <paul-alexis.rodriguez@universite-paris-saclay.fr>
2023-11-27 13:07:22 +0100

    [Typing] implemented the type checker for expressions and instructions.

--- typechecker.ml
+++ typechecker.ml
@@ -25,15 +25,51 @@ let typecheck_prog p =
   and type_expr e tenv =
     match e with
     | Int _ -> TInt
+    | Bool _ -> TBool
+    | Binop (exp, e1, e2)
+      when exp = Add || exp = Sub || exp = Mul || exp = Rem || exp = Div ->
+      check e1 TInt tenv ;
+      check e2 TInt tenv ;
+      TInt
+    | Binop (exp, e1, e2)
+      when exp = Lt || exp = Le || exp = Gt || exp = Ge || exp = Eq || exp = Neq
+      ->
+      check e1 TInt tenv ;
+      check e2 TInt tenv ;
+      TBool
+    | Binop (exp, e1, e2) when exp = Or || exp = And ->
+      check e1 TBool tenv ;
+      check e2 TBool tenv ;
+      TBool
+    | Unop (Opp, e1) ->
+      check e1 TInt tenv ;
+      TInt
+    | Unop (Not, e1) ->
+      check e1 TBool tenv ;
+      TBool
+    | Get s -> type_mem_access s tenv
     | _ -> failwith "case not implemented in type_expr"
   and type_mem_access m tenv =
     match m with
+    | Var s -> begin
+      try Env.find s tenv
+      with Not_found -> error (Printf.sprintf "Variable %s not found" s)
+    end
     | _ -> failwith "case not implemented in type_mem_access" in
 
   let rec check_instr i ret tenv =
     match i with
-    | Print e -> check e TInt tenv
+    | Print e -> check_multi e [TInt; TBool] tenv
+    | Set (m, e) -> check e (type_mem_access m tenv) tenv
     | _ -> failwith "case not implemented in check_instr"
+  and check_multi e type_list tenv =
+    begin
+      match type_list with
+      | [] -> error "wrong type for print"
+      | types :: type_list1 ->
+        let typ_e = type_expr e tenv in
+        if typ_e <> types then check_multi e type_list1 tenv
+    end
   and check_seq s ret tenv = List.iter (fun i -> check_instr i ret tenv) s in
 
   check_seq p.main TVoid tenv
